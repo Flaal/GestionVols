614e517a4691dc49ba6c29c8907c847564aadadf
Guilitane <jonvivlau@hotmail.fr>
2018-07-31 11:48:29 +0200

    Modification du Client (pour rajouter la liste de reservation, et création des trois classes dérivées ClientPhysique, ClientMoral, ClientEI

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.List;
+
 public abstract class Client {
 	private Integer id;
 	private String nom;
@@ -8,14 +10,8 @@ public abstract class Client {
 	private String email;
 	private Adresse adresse;
 	private Login login;
-	
-	
-	
-	
+	private List<Reservation> ListReservation;
 
-	
-	
-	
 	/**
 	 * @param id
 	 * @param nom
@@ -26,7 +22,7 @@ public abstract class Client {
 	 * @param login
 	 */
 	public Client(Integer id, String nom, String numeroTel, String numeroFax, String email, Adresse adresse,
-			Login login) {
+			Login login, List<Reservation> ListReservation) {
 		super();
 		this.id = id;
 		this.nom = nom;
@@ -35,11 +31,9 @@ public abstract class Client {
 		this.email = email;
 		this.adresse = adresse;
 		this.login = login;
+		this.ListReservation=ListReservation;
 	}
 
-
-	
-	
 	/**
 	 * @param nom
 	 * @param numeroTel
@@ -58,9 +52,6 @@ public abstract class Client {
 		this.login = login;
 	}
 
-
-
-
 	/**
 	 * 
 	 */
@@ -68,85 +59,69 @@ public abstract class Client {
 		super();
 	}
 
-
-
-
 	public Integer getId() {
 		return id;
 	}
-
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-
 	public String getNom() {
 		return nom;
 	}
-
 
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-
 	public String getNumeroTel() {
 		return numeroTel;
 	}
-
 
 	public void setNumeroTel(String numeroTel) {
 		this.numeroTel = numeroTel;
 	}
 
-
 	public String getNumeroFax() {
 		return numeroFax;
 	}
-
 
 	public void setNumeroFax(String numeroFax) {
 		this.numeroFax = numeroFax;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public Adresse getAdresse() {
 		return adresse;
 	}
-
 
 	public void setAdresse(Adresse adresse) {
 		this.adresse = adresse;
 	}
 
-
-
-
 	public Login getLogin() {
 		return login;
 	}
 
-
-
-
 	public void setLogin(Login login) {
 		this.login = login;
 	}
-	
-	
-	
-	
+
+	public List<Reservation> getListReservation() {
+		return ListReservation;
+	}
+
+	public void setListReservation(List<Reservation> listReservation) {
+		ListReservation = listReservation;
+	}
 	
 	
 }
---
+++ src/main/java/model/ClientMoral.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.List;
+
+public class ClientMoral extends Client{
+	
+	private String titreMoral; //Pourra être transformé en un objet Enum Titre, si du temps
+	private String siret;
+	
+	
+	public ClientMoral(Integer id, String nom, String numeroTel, String numeroFax, String email, Adresse adresse,
+			Login login, String titreMoral, String siret, List<Reservation> ListReservation) {
+		super(id, nom, numeroTel, numeroFax, email, adresse, login, ListReservation);
+		this.titreMoral = titreMoral;
+		this.siret = siret;
+	}
+	
+	public ClientMoral(String nom, String numeroTel, String numeroFax, String email, Adresse adresse,
+			Login login, String titreMoral, String siret, List<Reservation> ListReservation) {
+		super(nom, numeroTel, numeroFax, email, adresse, login, ListReservation);
+		this.titreMoral = titreMoral;
+		this.siret = siret;
+	}
+
+	public ClientMoral() {
+		super();
+	}
+
+	public String gettitreMoral() {
+		return titreMoral;
+	}
+
+	public void settitreMoral(String titreMoral) {
+		this.titreMoral = titreMoral;
+	}
+
+	public String getsiret() {
+		return siret;
+	}
+
+	public void setsiret(String siret) {
+		this.siret = siret;
+	}
+	
+	
+
+}
