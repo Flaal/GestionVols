cd446438e377bcfee71587d785085cd11917fc3e
Guilitane <jonvivlau@hotmail.fr>
2018-08-01 09:46:47 +0200

    DaoClientFactory mis à jour

--- src/main/java/dao/DaoClientFactory.java
+++ src/main/java/dao/DaoClientFactory.java
@@ -1,5 +1,11 @@
 package dao;
 
 public class DaoClientFactory {
+	private DaoClientFactory() {
+		// fait sauté le constructeur par défaut
+	}
 
+	public static DaoClient getInstance() {
+		return new DaoClientJdbcImpl();
+	}
 }
